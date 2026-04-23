119b104ac671490aafa84e95aba0ea0f1d56036d
Hexrtz <phatcharaponinpor22@gmail.com>
2026-04-23 20:01:55 +0700

    annoucement

--- models/Announcement.js
+++ models/Announcement.js
@@ -4,6 +4,11 @@ const AnnouncementSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     imageUrl: { type: String },
+    shop: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Shop',
+        required: true
+    }
 }, { timestamps: true }); // timestamps จะสร้าง createdAt และ updatedAt ให้อัตโนมัติ
 
 module.exports = mongoose.model('Announcement', AnnouncementSchema);
